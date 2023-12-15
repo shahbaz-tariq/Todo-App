a94e731b7d772ca401994bbbeb7aa324545b1b79
Shahbaz <shahbaztariqb@gmail.com>
2023-12-15 20:33:33 +0500

    CRUD Completed

--- app/src/main/java/com/example/todolist/AddTodo.kt
+++ app/src/main/java/com/example/todolist/AddTodo.kt
@@ -8,16 +8,12 @@ import android.text.TextUtils
 import com.example.todolist.databinding.ActivityAddTodoBinding
 
 class AddTodo : AppCompatActivity() {
-    lateinit var binding: ActivityAddTodoBinding
+    private lateinit var binding: ActivityAddTodoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTodoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        /*val title = binding.etTitle
-        val desc = binding.etDesc
-        val btn = binding.add*/
 
         when (intent.getStringExtra("type")) {
             "update" -> {
@@ -35,8 +31,8 @@ class AddTodo : AppCompatActivity() {
                         val desc = binding.etDesc.text.toString()
                         replyIntent.putExtra(EXTRA_REPLY, title)
                         replyIntent.putExtra(EXTRA_REPLY2, desc)
+                        replyIntent.putExtra("id", id)
                         setResult(2, replyIntent)
-                        intent.putExtra("id", id)
                     }
                     finish()
                 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,28 +1,19 @@
 package com.example.todolist
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.model.Todo
 
-/*import com.example.todolist.data.TodoDatabase
-import com.example.todolist.data.TodoRepo
-import com.example.todolist.model.Todo
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch*/
-
 class MainActivity : AppCompatActivity() {
 
-    //private val newWordActivityRequestCode = 1
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,26 +24,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        todoViewModel.allTodos.observe(this, Observer { todo ->
+        todoViewModel.allTodos.observe(this) { todo ->
             todo?.let { adapter.submitList(it) }
-        })
-
+        }
 
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddTodo::class.java)
-            intent.putExtra("type","addMode")
+            intent.putExtra("type", "addMode")
             startActivityForResult(intent, 1)
         }
-
-        //Sample code to check if the DB working perfectly, IT DOES!
-        /*val todoRepo = TodoRepo(TodoDatabase.getDatabase(this).todoDao())
-        val newTodo = Todo(0, "Hello World", "Database Completed")
-
-        GlobalScope.launch {
-            todoRepo.insert(newTodo)
-
-            todoRepo.getAllTodos()
-        }*/
 
         val swipeCallback = object :
             ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
@@ -72,12 +52,12 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     ItemTouchHelper.RIGHT -> {
-                        val intent: Intent = Intent(this@MainActivity,AddTodo::class.java)
-                        intent.putExtra("type","update")
-                        intent.putExtra("name",adapter.getTodo((viewHolder.adapterPosition)).name)
-                        intent.putExtra("desc",adapter.getTodo((viewHolder.adapterPosition)).desc)
-                        intent.putExtra("id",adapter.getTodo((viewHolder.adapterPosition)).id)
-                        startActivityForResult(intent,2)
+                        val intent = Intent(this@MainActivity, AddTodo::class.java)
+                        intent.putExtra("type", "update")
+                        intent.putExtra("name", adapter.getTodo((viewHolder.adapterPosition)).name)
+                        intent.putExtra("desc", adapter.getTodo((viewHolder.adapterPosition)).desc)
+                        intent.putExtra("id", adapter.getTodo((viewHolder.adapterPosition)).id)
+                        startActivityForResult(intent, 2)
                     }
                 }
             }
@@ -91,10 +71,10 @@ class MainActivity : AppCompatActivity() {
         TodoViewModel.TodoViewModelFactory((application as TodoApplication).repository)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        //if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
         if (requestCode == 1) {
             val reply1 = data?.getStringExtra(AddTodo.EXTRA_REPLY)
             val reply2 = data?.getStringExtra(AddTodo.EXTRA_REPLY2)
@@ -102,7 +82,7 @@ class MainActivity : AppCompatActivity() {
             if (reply1 != null && reply2 != null) {
                 val todo = Todo(0, reply1, reply2)
                 todoViewModel.insert(todo)
-                Toast.makeText(applicationContext, "Todo Inserted",Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Todo Inserted", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(
                     applicationContext,
@@ -110,16 +90,15 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_LONG
                 ).show()
             }
-        }
-        else if (requestCode == 2) {
+        } else if (requestCode == 2) {
             val reply1 = data?.getStringExtra(AddTodo.EXTRA_REPLY)
             val reply2 = data?.getStringExtra(AddTodo.EXTRA_REPLY2)
 
             if (reply1 != null && reply2 != null) {
-                val id = data.getIntExtra("id",0)
-                val todo = Todo(id,reply1, reply2)
+                val id = data.getIntExtra("id", 0)
+                val todo = Todo(id, reply1, reply2)
                 todoViewModel.update(todo)
-                Toast.makeText(applicationContext, "Todo Updated",Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Todo Updated", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(
                     applicationContext,
@@ -127,8 +106,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_LONG
                 ).show()
             }
-        }
-        else {
+        } else {
             Toast.makeText(
                 applicationContext,
                 R.string.empty_not_saved,
